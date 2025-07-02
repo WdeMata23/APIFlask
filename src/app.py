ffa7f77e1e8b2479325ba4c42986ab3f48cf1bea
Wilmer de Mata <wilmerdemata149@gmail.com>
2025-07-02 08:06:08 -0600

    Primera comit y subida de carpetas

---
+++ src/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from config import config
+from routes import Movie
+
+
+app = Flask(__name__)
+
+
+def pagina_no_encontrata(error):
+    return "<h1> pagina no encontrada </h1>", 404
+
+
+if __name__ == "__main__":
+    app.config.from_object(config["development"])
+
+    # error handlers
+    app.register_error_handler(404, pagina_no_encontrata)
+    app.run()
