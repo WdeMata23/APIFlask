1c863645141f44d21fd5fca73cd8cc6bca675151
Wilmer de Mata <wilmerdemata149@gmail.com>
2025-07-04 12:23:53 -0600

    Creacion de funcion Actualizar e Ingresar

--- src/routes/peliculas_routes.py
+++ src/routes/peliculas_routes.py
@@ -1,5 +1,10 @@
 from flask import Blueprint, jsonify
-from ..Controllers.peliculas_controller import get_pelicula, get_peliculas
+from ..Controllers.peliculas_controller import (
+    get_pelicula,
+    get_peliculas,
+    crear_pelicula,
+    actualizar_pelicula,
+)
 
 
 peliculas_bp = Blueprint("peliculas", __name__)
@@ -18,3 +23,13 @@ def get_all_peliculas_route():
 @peliculas_bp.route("/peliculas/<int:pelicula_id>", methods=["GET"])
 def get_single_pelicula_route(pelicula_id):
     return get_pelicula(pelicula_id)
+
+
+@peliculas_bp.route("/peliculas", methods=["POST"])
+def create_all_peliculas_route():
+    return crear_pelicula()
+
+
+@peliculas_bp.route("/peliculas/<int:pelicula_id>", methods=["PUT"])
+def actualizar_all_peliculas_route(pelicula_id):
+    return actualizar_pelicula(pelicula_id)
